ef897c5426797b622a26840b59a3e5db76ba4205
Suliman <salgaramanli2@gmail.com>
2024-03-28 06:39:27 +0200

    _UP_

--- Project/Code/v3(plot).py
+++ Project/Code/v3(plot).py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, auc
+import matplotlib.pyplot as plt
 
 # تعريف النموذج المتعلق بـ Naive Bayes
 class NaiveBayesModel:
@@ -18,6 +19,23 @@ class NaiveBayesModel:
         X_test = self.vectorizer.transform(X_test)  # تحويل النصوص إلى تمثيل رقمي
         return self.model.predict(X_test)  # التنبؤ بالتصنيف
 
+    def plot_roc_curve(self, X_test, y_test):
+        X_test = self.vectorizer.transform(X_test)  # تحويل النصوص إلى تمثيل رقمي
+        y_score = self.model.predict_proba(X_test)[:, 1]  # احتمالات الفئة الإيجابية
+        fpr, tpr, _ = roc_curve(y_test, y_score)
+        roc_auc = auc(fpr, tpr)
+
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic Curve')
+        plt.legend(loc="lower right")
+        plt.show()
+
 # تعريف النموذج المتعلق بـ Logistic Regression
 class LogisticRegressionModel:
     def __init__(self):
@@ -31,6 +49,23 @@ class LogisticRegressionModel:
     def predict(self, X_test):
         X_test = self.vectorizer.transform(X_test)  # تحويل النصوص إلى تمثيل رقمي
         return self.model.predict(X_test)  # التنبؤ بالتصنيف
+
+    def plot_roc_curve(self, X_test, y_test):
+        X_test = self.vectorizer.transform(X_test)  # تحويل النصوص إلى تمثيل رقمي
+        y_score = self.model.predict_proba(X_test)[:, 1]  # احتمالات الفئة الإيجابية
+        fpr, tpr, _ = roc_curve(y_test, y_score)
+        roc_auc = auc(fpr, tpr)
+
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic Curve')
+        plt.legend(loc="lower right")
+        plt.show()
 
 # تعريف الكلاس الرئيسي لتصنيف البريد الإلكتروني
 class EmailClassifier:
@@ -79,3 +114,10 @@ if __name__ == "__main__":
     for model_name, accuracies in results.items():
         print(f"\nModel: {model_name}")
         print("Average Accuracy:", sum(accuracies) / len(accuracies))  # طباعة متوسط الدقة لكل نموذج
+
+        # Plot ROC curve for each model
+        model = classifier.models[model_name]
+        model.plot_roc_curve(X, y)  # رسم بيان الفترة (ROC curve)
+
+
+           
---
+++ Project/Code/v4.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+class NaiveBayesModel:
+    def __init__(self):
+        self.model = MultinomialNB()
+        self.vectorizer = CountVectorizer()
+
+    def train(self, X_train, y_train):
+        X_train = self.vectorizer.fit_transform(X_train)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        X_test = self.vectorizer.transform(X_test)
+        return self.model.predict(X_test)
+
+class LogisticRegressionModel:
+    def __init__(self):
+        self.model = LogisticRegression(max_iter=1100)
+        self.vectorizer = CountVectorizer()
+
+    def train(self, X_train, y_train):
+        X_train = self.vectorizer.fit_transform(X_train)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        X_test = self.vectorizer.transform(X_test)
+        return self.model.predict(X_test)
+
+class EmailClassifier:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.data = None
+        self.models = {'Naive Bayes': NaiveBayesModel(), 'Logistic Regression': LogisticRegressionModel()}
+
+    def read_data(self):
+        self.data = pd.read_csv(self.data_path)
+
+    def clean_data(self):
+        self.data = self.data.fillna('')
+        self.data = self.data[self.data['text'].notna()]
+
+    def train_models(self):
+        results = {}
+        for model_name, model in self.models.items():
+            accuracies = []
+            X = self.data['text']
+            y = self.data['label']
+            model.train(X, y)
+            y_pred = model.predict(X)
+            accuracy = accuracy_score(y, y_pred)
+            accuracies.append(accuracy)
+            results[model_name] = accuracies
+
+            print(f"\nConfusion Matrix for {model_name}:")
+            print(confusion_matrix(y, y_pred))
+
+        return results
+
+    def predict_single_text(self, text):
+        nb_model = self.models['Naive Bayes']
+        nb_prediction = nb_model.predict([text])[0]
+
+        lr_model = self.models['Logistic Regression']
+        lr_prediction = lr_model.predict([text])[0]
+
+        return nb_prediction, lr_prediction
+
+
+if __name__ == "__main__":
+    data_path = "C:\\Users\\sulim\\Downloads\\DataSets_for_Spam-Emails\\combined_data.csv"
+    classifier = EmailClassifier(data_path)
+    classifier.read_data()
+    classifier.clean_data()
+    results = classifier.train_models()
+    for model_name, accuracies in results.items():
+        print(f"\nModel: {model_name}")
+        print("Average Accuracy:", sum(accuracies) / len(accuracies))
+
+    user_input = input("Enter the text to predict: ")
+    nb_prediction, lr_prediction = classifier.predict_single_text(user_input)
+    print("Naive Bayes Prediction:", nb_prediction)
+    print("Logistic Regression Prediction:", lr_prediction)
